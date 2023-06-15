c38c221926b25b4df52814430faada705be85a73
OlaTry <lila123456789ali@gmail.com>
2023-06-15 07:31:36 -0700

    advanced 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints with write function
+ *
+ * Return: 1 (not Success)
+*/
+
+int main(void)
+{
+write(1, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 62);
+return (1);
+}
