b279935a427280f94e7269d4a44e1b286a60b063
OlaTry <lila123456789ali@gmail.com>
2023-06-15 05:33:20 -0700

    upload task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
  * description: A C program that prints with puts function
  *
  * return: always 0 (success)
- *
 */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
