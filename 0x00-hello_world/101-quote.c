4fb52ed6630b5ad5c66bd746a0300e0d11bcaf0e
OlaTry <lila123456789ali@gmail.com>
2023-06-15 07:39:12 -0700

    uuuppp

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Description: A C program that prints with write function
- *
- * Return: 1 (not Success)
-*/
-
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", 62);
+write(2, "and that piece of art is useful - Dora Korpar, \" \"2015-10-19\n", 59);
 return (1);
 }
