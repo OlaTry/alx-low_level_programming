2bdbae473f1abcc86047335dbf618606dcde3b92
OlaTry <lila123456789ali@gmail.com>
2023-06-16 04:52:43 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print the alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+char a = 'a';
+while (a < 'z')
+{
+putchar(a);
+putch("\n");
+a++;
+}
+return (0);
+}
