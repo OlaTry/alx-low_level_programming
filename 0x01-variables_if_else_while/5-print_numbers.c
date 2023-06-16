4b04e14a4beefdd4dcddf38141be3cc43b278d39
OlaTry <lila123456789ali@gmail.com>
2023-06-16 06:32:25 -0700

    upload task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print the numbers from 0 : 9
+ *
+ * Return: 0 (success)
+*/
+
+
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+printf("\n");
+
+return (0);
+}
