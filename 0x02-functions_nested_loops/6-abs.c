ec8a73153bd21efd040779d6da972473f58477ff
OlaTry <lila123456789ali@gmail.com>
2023-06-20 11:23:42 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs -  computes the absolute value of an integer.
+ *
+ * @n:  computes the absolute value of an integer.
+ *
+ * Return: returns positive number
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	n *= -1;
+	}
+	return (n);
+}
