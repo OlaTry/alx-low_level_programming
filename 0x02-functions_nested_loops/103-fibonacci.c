2cca60f89d37832f596975dcc3a9b1f52d17e53a
OlaTry <lila123456789ali@gmail.com>
2023-06-21 02:01:47 -0700

    advanced 13

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: finds and prints the sum of the even-valued terms
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+float = total_sum;
+unsigned long  one = 0, two = 1, sum;
+while (1)
+{
+sum = one + two;
+if (sum > 4000000)
+	break;
+if ((sum % 2) == 0)
+	total_sum += sum
+one = two;
+two = sum;
+}
+printf("%.0f\n", total_sum);
+return (0);
+}
