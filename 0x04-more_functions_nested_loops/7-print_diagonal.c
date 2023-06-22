fd96c7573c58f4f2e77fb0ff321cbc50aa2df98b
OlaTry <lila123456789ali@gmail.com>
2023-06-22 07:23:55 -0700

    ola try task 7 again

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal -  function that draws a diagonal line
+ *
+ * @n: take input
+*/
+
+void print_diagonal(int n)
+{
+int p, s;
+
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (p = 1; p <= n; p++)
+{
+for (s = 1; s <= p; s++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
