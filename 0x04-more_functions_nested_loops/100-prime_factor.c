f34f58b620a5ac27b9d7a7728e6b46057982ab6e
OlaTry <lila123456789ali@gmail.com>
2023-06-22 03:18:35 -0700

    100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt - square root
+ *
+ * @x: take input
+ *
+ * Return: square root of x
+*/
+
+double _sqrt(double x)
+{
+float s, t;
+s = x / 2;
+t = 0;
+while (s != t)
+{
+t = s;
+s = (x / t + t) / 2;
+}
+return (s);
+}
+
+
+
+/**
+ * largest_prime_factor - finds and prints the largest
+ *
+ * @num: take input
+*/
+
+void largest_prime_factor(long int num)
+{
+int p, l;
+while (num % 2)
+	num = num / 2;
+for (p = 3; p <= _sqrt(num); p += 2)
+{
+while (num % p == 0)
+{
+num = num / p;
+l = p;
+}
+}
+if (num > 2)
+	l = num;
+printf("%d\n", l);
+}
+
+
+
+
+/**
+ * main -  Entry Point
+ *
+ * Return: always  0 (sucess)
+*/
+
+int main(void)
+{
+largest_prime_factor(612852475143);
+return (0);
+}
