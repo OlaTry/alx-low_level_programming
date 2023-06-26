94b36a03fcd3f079e7a353354e6634ffeba9e925
OlaTry <lila123456789ali@gmail.com>
2023-06-26 04:08:32 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 -  function that updates the value of points to to 98.
+ *
+ * @c: take input
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
