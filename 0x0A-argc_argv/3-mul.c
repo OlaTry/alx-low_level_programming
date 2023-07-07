1fbb0697960f92cf5d107ab3d7fa8caba92666c3
OlaTry <lila123456789ali@gmail.com>
2023-07-07 09:15:39 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point prints programm name
+ *
+ * @argc: int
+ *
+ * @argv: list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int s;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1] * atoi(argv[2]);
+		printf("%d\n", s);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
