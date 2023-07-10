6a845f10f12698e5cdcc3b4d01f2d4f969ef3f05
OlaTry <lila123456789ali@gmail.com>
2023-07-10 08:18:03 -0700

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -55,7 +55,7 @@ char **strtow(char *str)
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
-			for (j = 1; str[i + j] != ' ' && str[i + 1]; j++)
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 				;
 			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
