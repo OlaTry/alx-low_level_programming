499321b6a171221525e3047d087f6d8f0c70d2e4
OlaTry <lila123456789ali@gmail.com>
2023-07-13 06:23:36 -0700

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,10 +8,10 @@
  */
 void _puts(char *str)
 {
-        while (*str)
-        {
-                putchar(*str++);
-        }
+while (*str)
+{
+putchar(*str++);
+}
 }
 
 /**
@@ -23,7 +23,7 @@ void _puts(char *str)
  */
 int _isdigit(int c)
 {
-        return (c >= '0' && c <= '9');
+return (c >= '0' && c <= '9');
 }
 
 /**
@@ -35,22 +35,22 @@ int _isdigit(int c)
  */
 int _atoi(char *s)
 {
-        int result = 0;
-        int sign = 1;
+int result = 0;
+int sign = 1;
 
-        if (*s == '-')
-        {
-                sign = -1;
-                s++;
-        }
+if (*s == '-')
+{
+sign = -1;
+s++;
+}
 
-        while (_isdigit(*s))
-        {
-                result = result * 10 + (*s - '0');
-                s++;
-        }
+while (_isdigit(*s))
+{
+result = result * 10 + (*s - '0');
+s++;
+}
 
-        return result * sign;
+return (result *sign);
 }
 
 /**
@@ -60,18 +60,18 @@ int _atoi(char *s)
  */
 void print_int(int n)
 {
-        if (n < 0)
-        {
-                putchar('-');
-                n = -n;
-        }
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
 
-        if (n / 10)
-        {
-                print_int(n / 10);
-        }
+if (n / 10)
+{
+print_int(n / 10);
+}
 
-        putchar(n % 10 + '0');
+putchar(n % 10 + '0');
 }
 
 /**
@@ -85,17 +85,17 @@ void print_int(int n)
  */
 int main(int argc, char const *argv[])
 {
-        if (argc != 3 || !_isdigit(*argv[1]) || !_isdigit(*argv[2]))
-        {
-                _puts("Error\n");
-                return (98);
-        }
+if (argc != 3 || !_isdigit(*argv[1]) || !_isdigit(*argv[2]))
+{
+_puts("Error\n");
+return (98);
+}
 
-        int num1 = _atoi(argv[1]);
-        int num2 = _atoi(argv[2]);
+int num1 = _atoi(argv[1]);
+int num2 = _atoi(argv[2]);
 
-        print_int(num1 * num2);
-        putchar('\n');
+print_int(num1 * num2);
+putchar('\n');
 
-        return (0);
+return (0);
 }
