dc8fdddf473b14ec60a4222bf35afddb560d3995
OlaTry <lila123456789ali@gmail.com>
2023-07-13 09:04:23 -0700

    try

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+
 #endif
