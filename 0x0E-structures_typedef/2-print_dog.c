961878bd8df222a20c29a7fa78a23e94b3668ebb
OlaTry <lila123456789ali@gmail.com>
2023-07-17 05:56:34 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name ? d->name : "(ni)");
+		printf("Name: %s\n", d->name ? d->name : "(ni1)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner ? d->owner : "(ni)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(ni1)");
 	}
 }
