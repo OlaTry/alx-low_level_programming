ca464e1cda32e42b4762f7738f6c325556928191
OlaTry <lila123456789ali@gmail.com>
2023-07-27 03:01:55 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a node to the start of the list 
+ * add_node - adds a node to the start of the list
  * @head: address of pointer
  * @str: str field of node
  *
